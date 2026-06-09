e8c61ad9985e8432c325492a852a308dcb6ccb0b
Karthik C <121020417+Drakey504@users.noreply.github.com>
2026-06-09 10:47:01 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        hash = defaultdict(list)
+
+        for s in strs:
+            freq_list = [0] * 26
+            for c in s:
+                freq_list[(ord(c)-97)]+=1
+
+            hash[tuple(freq_list)].append(s)
+
+        return list(hash.values())
+
+
+        
